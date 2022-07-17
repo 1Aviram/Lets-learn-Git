965e4dc7ae904f1fd494ebb01d76d13f043db0cc
Aviram Mandal <avirammandal1@gmail.com>
2022-07-17 12:57:02 +0530

    COOMMIT OF DIFF

---
+++ Diff.py
@@ -0,0 +1,8 @@
+from ast import Sub
+from re import sub
+
+
+num1 = 15
+num2 = 40
+Sub = num1 - num2 
+print("Sum of two numbers =",sub)
--- sum.py
+++ sum.py
@@ -1,4 +1,4 @@
 num1 = 15
-num2 = 450
+num2 = 46
 sum = num1 + num2 
 print("Sum of two numbers =",sum )
